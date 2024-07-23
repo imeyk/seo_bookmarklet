e82cccaab9f1f5b8edb35367f4c48e6cd965afcd
Oleg Peshkov <70931001+imeyk@users.noreply.github.com>
2024-07-23 23:56:28 +0500

    Update and rename url.link to url.js

--- url.js
+++ url.js
@@ -1,5 +1,5 @@
 javascript: (function() {
-            var scriptSrc = 'https://imeyk.ru/bookmarklet.js';
+            var scriptSrc = 'https://cdn.jsdelivr.net/gh/imeyk/seo_bookmarklet/bookmarklet.js';
             if (!document.querySelector('script[src="' + scriptSrc + '"]')) {
                 var script = document.createElement('script');
                 script.src = scriptSrc;
